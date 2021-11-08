ddd4bfad226338fd98bb4b52ec7bf30cb34b169e
vhalvarez <alvarez.programmer@gmail.com>
2021-11-08 11:44:13 -0400

    Crear y ejecutar migraciones

--- djangovue/settings.py
+++ djangovue/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
+        'NAME': 'djangovueelement',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
